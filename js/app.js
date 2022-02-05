6583fc1ec6e3cd2d176bbef2ec4c1e22d1cad9a7
awatersny <awatersny@gmail.com>
2022-02-05 15:01:13 -0500

    Initialize variables in init function

--- js/app.js
+++ js/app.js
@@ -17,26 +17,24 @@ const boardSq = document.querySelectorAll(".square");
 
 /*-------------------------------- Functions --------------------------------*/
 function init(){
-
-}
-// 3) Upon loading, the app should:
-
-	// 3.1) Call an initialize function
-
 	// 3.2) That initialize function should initialize the state variables:
-	  // 3.2.1) Initialize the board array to 9 nulls to represent empty squares. 
+	  // 3.2.1) Initialize the board array to 9 nulls to represent empty squares.
 	    // The 9 elements will "map" to each square.
+      squares = [null, null, null, null, null, null, null, null, null];
 	    // Index 0 represents the top-left square.
 	    // Index 1 represents the top-middle square.
 			// So on, continuing through the entire board until...
 	    // Index 8 maps to the bottom-right square.
 	  // 3.2.2) Initialize whose turn it is to 1 (player 'X'). 
 	    // Player 'O' will be represented by -1.
+      turn = 1;
 	  // 3.2.3) Initialize the winner variable to null.
 	    // This represents that there is no winner or tie yet. 
 	    // The winner variable will hold the player value (1 or -1) if there's a winner. 
 	    // The winner will hold a 'T' if there's a tie.
+      winner = null;
 	  // 3.2.4) Render those state variables to the page by calling a render function.
+}
 
 	// 3.3) The render function should:
 	  // 3.3.1) Loop over the board array (which represents the squares on the page), and for each iteration:
